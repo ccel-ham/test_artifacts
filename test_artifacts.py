2c2bb54683a7bdb32fa2692f6bee665be9ceb81f
ccel-ham <90171055+ccel-ham@users.noreply.github.com>
2022-09-28 21:15:31 +0900

    Update test_artifacts.py

--- test_artifacts.py
+++ test_artifacts.py
@@ -1,3 +1,23 @@
-f = open('myfile.txt', 'w', encoding="shift-jis")
-f.write('0')
-f.close()
+import os
+
+def main():
+    path = './myfile.txt'
+    is_file = os.path.isfile(path)
+    if is_file:
+        f = open('myfile.txt', 'r', encoding="utf-8")
+        s = f.read()
+        f.close()
+    else:
+        s="0"
+
+    print(f"open:{s}")
+    if s == "":
+        s = 1
+    else:
+        s = int(s)+1
+    f = open('myfile.txt', 'w', encoding="utf-8")
+    f.write(str(s))
+    f.close()
+
+if __name__ == '__main__':
+    main()
